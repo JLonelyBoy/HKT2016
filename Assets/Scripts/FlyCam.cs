0062d835ff3ea500b756b1221ef231144a74a665
Vince <vincewang10a7@gmail.com>
2016-03-20 08:45:51 +0900

    fix canvas

---
+++ Assets/Scripts/FlyCam.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyCam : MonoBehaviour {
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("FlyCam"), "time", 5, "aesetype", iTween.EaseType.easeInOutSine));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+	
+	}
+}
