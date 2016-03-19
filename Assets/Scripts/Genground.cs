ddcfc283d91aead91491d4ccd3f8500ca963c8ec
Vince <vincewang10a7@gmail.com>
2016-03-19 15:04:33 +0900

    Folder structure

---
+++ Assets/Scripts/Genground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class Genground : MonoBehaviour {
+
+    public int vsize;
+    public int hsize;
+    GameObject _bMaterial;
+    GameObject _wMaterail;
+	// Use this for initialization
+	void Start () {
+        _bMaterial = AssetDatabase.LoadAssetAtPath("",typeof(GameObject));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
