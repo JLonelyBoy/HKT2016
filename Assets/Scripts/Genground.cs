3aa45db5d1d725a5d7cfd86ef85506221a6133bc
Vince <vincewang10a7@gmail.com>
2016-03-19 15:31:41 +0900

    Test

--- Assets/Scripts/Genground.cs
+++ Assets/Scripts/Genground.cs
@@ -6,13 +6,27 @@ public class Genground : MonoBehaviour {
 
     public int vsize;
     public int hsize;
-    GameObject _bMaterial;
-    GameObject _wMaterail;
+    Material _bMaterial;
+    Material _wMaterail;
+    GameObject _pref;
 	// Use this for initialization
 	void Start () {
-        _bMaterial = AssetDatabase.LoadAssetAtPath("",typeof(GameObject));
-	
-	}
+        _bMaterial = Resources.Load("Materials/BlackGround.mat") as Material;
+        _wMaterail = Resources.Load("Materials/WhiteGround.mat") as Material;
+        
+
+    }
+
+    void init()
+    {
+        initGround();
+
+    }
+
+    void initGround()
+    {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
