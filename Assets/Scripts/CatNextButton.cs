ccc5fb51462841a475bdfaf04878a94cf41869a0
武田 洋 <h-takeda@gcrest.com>
2016-03-19 21:43:44 +0900

    猫testコミット

---
+++ Assets/Scripts/CatNextButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatNextButton : MonoBehaviour {
+
+	public void OnClick() {
+		Application.LoadLevel ("CatScene");
+	}
+}
---
+++ Assets/Scripts/EventButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventButton : MonoBehaviour {
+
+	public void OnClick() {
+		gameObject.SetActive(false);
+	}
+}
